701724be7ac76a3fe270f55d257696ba7a584328
Edson Junior <edsonmsilvajr@gmail.com>
2025-09-03 20:12:39 -0300

    adiciona onSaveInstanceState e onRestoreInstanceState

--- app/src/main/java/br/edu/ifsc/edson/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/edson/MainActivity.java
@@ -2,11 +2,13 @@ package br.edu.ifsc.edson;
 
 import android.app.LocaleManager;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -41,12 +43,18 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
-        getString(R.string.hello);
-        LocaleManager local;
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putString("Sorteado", textViewResultado.getText().toString());
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        textViewResultado.setText(savedInstanceState.getString("Sorteado"));
     }
 }
