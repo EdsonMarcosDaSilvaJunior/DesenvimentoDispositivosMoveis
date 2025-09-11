8a8c91c2bb75f2e40e98a004e90ac5722d8382c5
Edson Junior <edsonmsilvajr@gmail.com>
2025-09-10 21:15:22 -0300

    Adiciona Fragmento B

--- app/src/main/java/br/edu/ifsc/edson/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/edson/MainActivity.java
@@ -1,6 +1,7 @@
 package br.edu.ifsc.edson;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
@@ -9,12 +10,15 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
     FrameLayout frameLayout;
+    Button buttonA, buttonB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         frameLayout=findViewById(R.id.frameLayout);
+        buttonA=findViewById(R.id.fragmentoA);
+        buttonB=findViewById(R.id.fragmentoB);
 
         FragmentA fragmentA = new FragmentA();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
