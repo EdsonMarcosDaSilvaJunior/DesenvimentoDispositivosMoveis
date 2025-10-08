8946c0922cd01cdedc7b45f3ed0eb496127ecc43
Edson Junior <edsonmsilvajr@gmail.com>
2025-10-08 19:46:49 -0300

    Adiciona Listagem de nomes com botão para adição

--- app/src/main/java/br/edu/ifsc/edson/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/edson/MainActivity.java
@@ -2,6 +2,11 @@ package br.edu.ifsc.edson;
 
 import android.app.LocaleManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,19 +14,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    EditText editTextNome;
+    Button button;
+    ArrayList<String> nomes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.hello);
-        LocaleManager local;
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        listView =findViewById(R.id.listView);
+        editTextNome = findViewById(R.id.editTextNome);
+        button = findViewById(R.id.adicionarNome);
+
+        nomes = new ArrayList<>(){{
+            add("Edson");
+            add("Augusto");
+            add("Jorge");
+            add("Roberto");
+        }};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes
+        );
+        listView.setAdapter(adapter);
+
+        button.setOnClickListener(v -> {
+            nomes.add(editTextNome.getText().toString());
+            listView.setAdapter(adapter);
+            editTextNome.setText("");
         });
+
     }
 }
