e10b763e64a83709025d5c46ede82a80bbfc7799
Edson Junior <edsonmsilvajr@gmail.com>
2025-11-05 20:16:42 -0300

    Adiciona listagem sqlite

--- app/src/main/java/br/edu/ifsc/edson/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/edson/MainActivity.java
@@ -1,7 +1,14 @@
 package br.edu.ifsc.edson;
 
 import android.app.LocaleManager;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,19 +16,65 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase database;
+    EditText editText;
+    Button saveButton;
+    ListView listView;
+    ArrayList<String> usersList;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.hello);
-        LocaleManager local;
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editText = findViewById(R.id.editTextText);
+        saveButton = findViewById(R.id.BtnSalvar);
+        listView = findViewById(R.id.ListName);
+
+        ArrayList<String> usersList = new ArrayList<>();
+
+
+        //path
+        database = openOrCreateDatabase("app_database", MODE_PRIVATE, null);
+        database.execSQL("CREATE TABLE IF NOT EXISTS users ("+
+                "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "name VARCHAR, texto VARCHAR)");
+
+
+        saveButton.setOnClickListener(v -> {
+            String texto = editText.getText().toString();
+            if(!texto.isEmpty()){
+                ContentValues contentValues = new ContentValues();
+
+                contentValues.put("name", texto);
+                contentValues.put("texto", texto);
+
+                database.insert("users",null,contentValues);
+            }
         });
+
+        carregarUser();
+    }
+    public void carregarUser(){
+        usersList.clear();
+        Cursor cursor = database.rawQuery("SELECT * FROM users", null);
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()){
+            String s = cursor.getString(cursor.getColumnIndex("name"));
+            usersList.add(s);
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, usersList);
+        listView.setAdapter(adapter);
+
     }
 }
