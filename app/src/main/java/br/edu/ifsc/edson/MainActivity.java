cc60a5806e6201f719d2601d854ae06bf639cc23
Edson Junior <edsonmsilvajr@gmail.com>
2025-11-12 21:51:21 -0300

    Adiciona sensor de luz

--- app/src/main/java/br/edu/ifsc/edson/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/edson/MainActivity.java
@@ -1,7 +1,13 @@
 package br.edu.ifsc.edson;
 
 import android.app.LocaleManager;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,19 +15,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    SensorManager sm;
+
+    Sensor sensorLuz;
+    TextView tvLuz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.hello);
-        LocaleManager local;
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tvLuz=findViewById(R.id.textView);
+
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorLuz = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        sm.registerListener(MainActivity.this, sensorLuz, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float luz = event.values[0];
+        tvLuz.setText(luz+" lz");
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
