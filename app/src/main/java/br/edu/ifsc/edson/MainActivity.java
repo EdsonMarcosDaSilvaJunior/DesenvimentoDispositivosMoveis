0f5fd9cdaf5f1ea3c73ef1cf2a135b67f3fee808
Edson Junior <edsonmsilvajr@gmail.com>
2025-11-18 21:00:09 -0300

    Inicia PrecificacaoCroche adicionando tela inicial e começando Dados

--- app/src/main/java/br/edu/ifsc/edson/MainActivity.java
+++ app/src/main/java/br/edu/ifsc/edson/MainActivity.java
@@ -2,6 +2,7 @@ package br.edu.ifsc.edson;
 
 import android.app.LocaleManager;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +11,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonConfig;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.hello);
-        LocaleManager local;
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        buttonConfig = findViewById(R.id.buttonConfig);
+
+        buttonConfig.setOnClickListener(v-> {
+          });
     }
 }
